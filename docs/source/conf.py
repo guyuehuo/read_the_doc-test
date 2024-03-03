2893f5c693755a16f827ea303074861a5bebc0c8
guyuehuo <70420236+guyuehuo@users.noreply.github.com>
2024-03-03 20:58:09 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'Opendock'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Huqiuyue'
 
 release = '0.1'
 version = '0.1.0'
